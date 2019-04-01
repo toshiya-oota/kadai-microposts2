1b8d3b7971eb43993db14b78241377ec3c42bea5
toshiya.oota <toshiya.1988.ota@gmail.com>
2019-04-01 13:31:31 +0000

    likedestroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  
   validates :content, presence: true, length: { maximum: 255 }
   
-  has_many :favorites
+  has_many :favorites, class_name: 'Favorite', foreign_key: 'like_id', dependent: :destroy
   has_many :likers, through: :favorites, source: :user
 end
